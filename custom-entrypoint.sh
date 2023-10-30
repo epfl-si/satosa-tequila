dd286a3e8d8be35340acd825c38862aec8a90a50
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2023-10-30 08:59:28 +0100

    [feature] 12-factored `SATOSA_ENTITY_ID` and `SATOSA_BASE_URL`
    Eschew SATOSA's `!ENV` YAML tag ([documented here](https://github.com/IdentityPython/SATOSA/blob/master/doc/README.md#configuration)) in favor of plain old copy + `sed`, as the former doesn't support expansion

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -2,6 +2,24 @@
 
 . /usr/local/bin/docker-entrypoint.sh
 
+twelve_factorify_config () {
+	cp /proxy_conf.yaml /etc/satosa/
+	cp -a /config /etc/satosa/config
+
+        if test -v SATOSA_ENTITY_ID; then
+		sed -i -e "s|entityid: https://satosa-127-0-0-1.nip.io/tequila|entityid: ${SATOSA_ENTITY_ID}|" /etc/satosa/config/saml2_backend.yaml
+	fi
+
+	if test -v SATOSA_BASE_URL; then
+		for twelve_factorable in /etc/satosa/config/* /etc/satosa/config/*/* ; do
+			sed -i -e "s|https://satosa-127-0-0-1.nip.io|${SATOSA_BASE_URL}|" \
+				$twelve_factorable;
+		done
+	else
+		export SATOSA_BASE_URL=https://satosa-127-0-0-1.nip.io
+	fi
+}
+
 unset -f docker_pprint_metadata
 # Doctored because the original insists on `frontend.xml` being a correct XML file,
 # which it won't because we don't use SAML as front-end.
@@ -18,4 +36,5 @@ docker_pprint_metadata () {
 	mv backend.xml.new backend.xml
 }
 
+twelve_factorify_config
 _main gunicorn --certfile=/tls/satosa.crt --keyfile=/tls/satosa.key -b0.0.0.0:8443 epfl.satosa_wsgi:app
