ec8d2c472f7846e97f38ef9c85d5262c6aab9552
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2023-11-06 17:32:13 +0100

    [fix] an `attributemaps` SNAFU or two
    - Copy into the proper directory, DUH
    - Nuke any `__pycache__`

--- custom-entrypoint.sh
+++ custom-entrypoint.sh
@@ -10,7 +10,8 @@ twelve_factorify_config () {
 	cp -a /config/* /etc/satosa/config
 	mkdir -p /etc/satosa/config/attributemaps || true
 	if [ -d "/attributemaps" ]; then
-		cp -a /attributemaps/* /etc/satosa/config
+		cp -a /attributemaps/* /etc/satosa/config/attributemaps/
+                rm -rf /etc/satosa/config/attributemaps/__pycache__ || true
 	fi
 
 	if test -v SATOSA_ENTITY_ID; then
